c4199bc4249219926982185b9fb79240ead159bf
palomaguiral <pguiort@gmail.com>
2023-10-11 15:47:43 +0200

    autenticación con google

--- backend/auth/auth_utils.py
+++ backend/auth/auth_utils.py
@@ -1,20 +1,21 @@
 def get_authenticated_user_details(request_headers):
     user_object = {}
 
-    ## check the headers for the Principal-Id (the guid of the signed in user)
-    if "X-Ms-Client-Principal-Id" not in request_headers.keys():
-        ## if it's not, assume we're in development mode and return a default user
-        from . import sample_user
-        raw_user_object = sample_user.sample_user
+    ## Comprueba las cabeceras en busca de la clave específica de Google
+    if "X-Google-Client-Principal-Id" not in request_headers:
+        ## Si no se encuentra, puedes manejarlo como desees, por ejemplo, regresar un usuario predeterminado.
+        # from . import sample_user
+        # raw_user_object = sample_user.sample_user
+        return None
     else:
-        ## if it is, get the user details from the EasyAuth headers
-        raw_user_object = {k:v for k,v in request_headers.items()}
+        ## Si se encuentra, obtén los detalles del usuario desde las cabeceras de Google
+        raw_user_object = {k: v for k, v in request_headers.items()}
 
-    user_object['user_principal_id'] = raw_user_object['X-Ms-Client-Principal-Id']
-    user_object['user_name'] = raw_user_object['X-Ms-Client-Principal-Name']
-    user_object['auth_provider'] = raw_user_object['X-Ms-Client-Principal-Idp']
-    user_object['auth_token'] = raw_user_object['X-Ms-Token-Aad-Id-Token']
-    user_object['client_principal_b64'] = raw_user_object['X-Ms-Client-Principal']
-    user_object['aad_id_token'] = raw_user_object["X-Ms-Token-Aad-Id-Token"]
+    user_object['user_principal_id'] = raw_user_object['X-Google-Client-Principal-Id']
+    user_object['user_name'] = raw_user_object['X-Google-Client-Principal-Name']
+    user_object['auth_provider'] = 'Google'  # Puedes establecer el proveedor como "Google".
+    user_object['auth_token'] = raw_user_object['X-Google-Token']  # Ajusta esto según las cabeceras de Google.
+    user_object['client_principal_b64'] = raw_user_object['X-Google-Client-Principal']  # Ajusta esto según las cabeceras de Google.
+    user_object['google_id_token'] = raw_user_object["X-Google-ID-Token"]  # Ajusta esto según las cabeceras de Google.
 
     return user_object
